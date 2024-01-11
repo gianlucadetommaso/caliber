62b9ce7216b050793a984d1e3d30ab44669cc1a1
Gianluca Detommaso <detommaso.gianluca@gmail.com>
2024-01-11 15:12:09 +0100

    add lower bound

--- caliber/binary_classification/linear_scaling/smooth_fit_mixin_linear_scaling.py
+++ caliber/binary_classification/linear_scaling/smooth_fit_mixin_linear_scaling.py
@@ -1,3 +1,5 @@
+from typing import List, Optional, Tuple
+
 import numpy as np
 
 from caliber.binary_classification.base_smooth_fit_mixin import (
@@ -8,5 +10,13 @@ from caliber.binary_classification.base_smooth_fit_mixin import (
 class BinaryClassificationLinearScalingSmoothFitMixin(
     BinaryClassificationSmoothFitMixin
 ):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if "bounds" not in self._minimize_options:
+            self._minimize_options["bounds"] = self._get_bounds()
+
     def _get_x0(self) -> np.ndarray:
         return np.array([0.0, 1.0]) if self._has_intercept else np.array(1.0)
+
+    def _get_bounds(self) -> List:
+        return [(None, None), (0.0, None)] if self._has_intercept else [(0.0, None)]
