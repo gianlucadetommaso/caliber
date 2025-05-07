b3e0112daf2adc9124bae2538549bf1286dac3e4
Gianluca Detommaso <gianluca.detommaso@helsing.ai>
2025-05-07 16:19:31 +0200

    add file

---
+++ caliber/binary_classification/minimizing/mixins/pred_from_probs_mixin.py
@@ -0,0 +1,16 @@
+import numpy as np
+from typing import Optional
+
+from caliber.binary_classification.checks_mixin import BinaryClassificationChecksMixin
+
+
+class PredFromProbsBinaryClassificationMixin(BinaryClassificationChecksMixin):
+    def __init__(self, threshold: float, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.threshold = threshold
+
+    def predict(self, probs: np.ndarray, features: Optional[np.ndarray] = None) -> np.ndarray:
+        self._check_probs(probs)
+        self._check_features(features)
+        probs = self._predict_proba(self._params, probs, features)
+        return (probs > self.threshold).astype(int)
